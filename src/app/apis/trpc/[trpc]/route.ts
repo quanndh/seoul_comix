84097a999589408639fb09067c3739becb60cb2f
Lucas Nguyen <lucas@ideabox.com.au>
2024-06-27 10:31:24 +0700

    init

--- src/app/apis/trpc/[trpc]/route.ts
+++ src/app/apis/trpc/[trpc]/route.ts
@@ -5,6 +5,24 @@ import {
 import { appRouter } from "../trpc-router";
 
 const handler = (request: Request) => {
+  request.headers.append("Access-Control-Allow-Credentials", "true");
+  request.headers.append(
+    "Access-Control-Allow-Origin",
+    "https://seoul-comix.vercel.app"
+  );
+  request.headers.append("Access-Control-Allow-Origin", "localhost:3000");
+  request.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  request.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  console.log(request.headers);
+
+
   return fetchRequestHandler({
     endpoint: "/apis/trpc",
     req: request,
@@ -16,5 +34,6 @@ const handler = (request: Request) => {
     },
   });
 };
+
 
 export { handler as GET, handler as POST };
